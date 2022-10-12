e97d48ce197409d803ed73c9ea71f4b0afd21dae
w0989en <w0989en@yahoo.com.tw>
2022-10-13 00:29:23 +0800

    3

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -1,14 +1,23 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
+//    @Autowired
+//    private Printer printer;
+
+    private final Printer printer;
+    public MyController(Printer printer) {
+        this.printer = printer;
+    }
+
     @RequestMapping("/test")
     public String test(){
-        System.out.println("MyController");
+        printer.print("aaaa");
         return "Hi~~~~~";
     }
 }
