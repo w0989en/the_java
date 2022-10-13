ed7e6a74f47969431dac2bcfa40b16001549e940
w0989en <w0989en@yahoo.com.tw>
2022-10-14 00:45:40 +0800

    3

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -1,19 +1,21 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
-//    @Autowired
-//    private Printer printer;
+    @Autowired
+    @Qualifier("canonPrinter")
+    private Printer printer;
 
-    private final Printer printer;
-    public MyController(Printer printer) {
-        this.printer = printer;
-    }
+//    private final Printer printer;
+//    public MyController(@Qualifier("canonPrinter") Printer printer) {
+//        this.printer = printer;
+//    }
 
     @RequestMapping("/test")
     public String test(){
